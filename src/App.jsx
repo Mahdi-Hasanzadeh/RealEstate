03dc800acde802391af1f99c5f5e1ff1e0a0c400
Mahdi <mahdi.h2619@gmail.com>
2024-03-18 13:12:06 -0700

    routes bugs fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // import { Suspense, lazy } from "react";
-import { Outlet, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 
 import { Navbar, Home, About } from "../public/Components/ComponentsReturn";
 import { Box } from "@mui/material";
@@ -16,31 +16,22 @@ const App = () => {
   return (
     <>
       <Box>
-        <Routes>
-          <Route path="/" element={<BasicLayout />}>
-            <Route index element={<Home />} />
-            <Route
-              path="/about"
-              element={
-                <div>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Exercitationem amet nam dolores sequi, praesentium ullam rem
-                  eum in, minima fugit odio facilis quibusdam. Omnis nam
-                  possimus corrupti vitae cum maxime iste iure laboriosam culpa
-                  eligendi deleniti delectus assumenda inventore alias est
-                  ducimus, dolore distinctio perferendis placeat et sapiente.
-                  Excepturi similique sed in reiciendis, magnam debitis
-                  doloremque. Reiciendis officia ea hic eligendi, at
-                  exercitationem sunt nesciunt sequi consequuntur soluta nemo
-                  nihil voluptatum numquam doloribus? Dolores sed asperiores
-                  ullam tenetur exercitationem labore est expedita alias modi
-                  magnam deleniti omnis ad laboriosam ea eveniet, cum,
-                  voluptatum totam! Odio adipisci unde veniam quo temporibus!
-                </div>
-              }
-            />
-          </Route>
-        </Routes>
+        <BrowserRouter basename="/RealEstate/">
+          <Routes>
+            <Route path="/" element={<BasicLayout />}>
+              <Route index element={<Home />} />
+              <Route
+                path="/about"
+                element={
+                  <div>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Exercitationem amet nam dolores sequi, praesentium ullam rem
+                  </div>
+                }
+              />
+            </Route>
+          </Routes>
+        </BrowserRouter>
       </Box>
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,6 @@ import { BrowserRouter, HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/RealEstate">
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
